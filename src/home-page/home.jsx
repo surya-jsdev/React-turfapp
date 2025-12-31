26f0306f26046290e5aacdd77972106bfbe57fd6
SURYA-200346 <surya.jsdev@gmail.com>
2025-12-31 19:37:21 +0530

    login ,sign up page added

---
+++ src/home-page/home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+function Home() {
+
+
+    const style = {
+        padding: "8px",
+        margin: "5px",
+        color: "white",
+        border:"1px solid white",
+    }
+    return (
+        <>
+            <div className='p-10  w-full flex justify-around'>
+                <h2 className='text-white text-[1.5em]'><span className='text-[red]'>Roger</span>Turf</h2>
+                <div>
+                    <button style={style}>Login</button>
+                    <button style={style}>Signup</button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import './index.css'
+import './index.css'
 import App from './App.jsx'
-import Home from './home.jsx'
+import Home from './home-page/home.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
