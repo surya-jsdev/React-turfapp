2e50392193adde82982105390f2815b840c6f6d8
SURYA-200346 <surya.jsdev@gmail.com>
2026-01-02 21:55:30 +0530

    added css style for navbar

--- src/home-page/home.jsx
+++ src/home-page/home.jsx
@@ -3,20 +3,23 @@ function Home() {
 
 
     const style = {
-        padding: "8px",
+        padding: "10px",
         margin: "5px",
         color: "white",
         border: "1px solid white",
+        borderRadius: "10px",
     }
     return (
         <>
-            <div className='p-10  w-full flex justify-around'>
-                <h2 className='text-white text-[1.5em]'><span className='text-[red]'>ROGER
-                </span>ACADEMY</h2>
-                <div>
-                    <button style={style}>Login</button>
+            <div className='p-5  w-full flex justify-between bg-[#0f172a] border-b-1 border-[#253144] fixed'>
+                <h2 className='text-white text-[1.5em] font-bold'><span className='text-[#8257e7] p-2'>ROGER</span>ACADEMY</h2>
+                <div className=''>
                     <button style={style}>Signup</button>
+                    <button style={style} className='bg[#8b5cf6]'>Login</button>
                 </div>
+            </div>
+            <div className='w-full h-140 bg-[#1b2537]'>
+
             </div>
         </>
     )
--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import Login from './Login.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-    {/* <Home /> */}
-    <Login/>
+    <Home />
+    {/* <Login/> */}
   </StrictMode>,
 )
