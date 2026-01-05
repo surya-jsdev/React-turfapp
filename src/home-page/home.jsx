36b5cadc07e238c7ad644b7daa2239583cc07ff5
SURYA-200346 <surya.jsdev@gmail.com>
2026-01-05 11:43:34 +0530

    turf data added

--- src/home-page/home.jsx
+++ src/home-page/home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { turf } from '../services/api'
 function Home() {
     const style = {
         padding: "10px",
@@ -13,15 +14,23 @@ function Home() {
                 <h2 className='text-white text-[1.5em] font-bold'><span className='text-[#8257e7] p-2'>ROGER</span>ACADEMY</h2>
                 <div className=''>
                     <button style={style}>Signup</button>
-                    <button style={style} className='bg[#8b5cf6]'>Login</button>
+                    <button style={style} className='bg-[#8b5cf6] w-[100px]'>Login</button>
                 </div>
             </div>
-            <section className='w-full h-140 bg-[#1b2537] flex justify-center items-center'>
-                <div>
+            <section className='w-full h-160 bg-[#1b2537] flex justify-center items-center'>
+                <div className='flex justify-center flex-wrap w-xl'>
                     <h1 className='text-[3.5rem] text-white text-center'>Book Your Game <br />Own the Turf</h1>
-                     <p className="text-[1.2rem] text-white text-center w-[600px]" >
+                     <p className="text-[1.2rem] text-white text-center w-[600px] text-[#7b90a8]" >
                         Premium sports venues for Football, Cricket, and Pickleball. Instant booking, zero hassle.
                     </p>
+                    <button className='mt-4 text-white bg-[#8b5cf6] p-3 rounded-xl'>
+                        Explore Venues
+                    </button>
+                </div>
+            </section>
+            <section>
+                <div>
+                   
                 </div>
             </section>
         </>
---
+++ src/services/api.js
@@ -0,0 +1,27 @@
+export const turf = [{
+    id: 1,
+    name: "Thunder Bolt Arena",
+    sport: "Football",
+    image: "https://images.unsplash.com/photo-1552667466-07770ae110d0?auto=format&fit=crop&q=80&w=800",
+    location: "Downtown Sports Complex",
+    price: 1200,
+    rating: 4.8,
+}, {
+    id: 2,
+    name: "Green Valley Pitch",
+    sport: "Football",
+    image: "https://images.unsplash.com/photo-1529900748604-07564a03e7a6?auto=format&fit=crop&q=80&w=800",
+    location: "Green Valley",
+    price: 1000,
+    rating: 4.5,
+},
+{
+    id: 3,
+    name: "Master Blaster Nets",
+    sport: "Cricket",
+    image: "https://images.unsplash.com/photo-1531415074968-036ba1b575da?auto=format&fit=crop&q=80&w=800",
+    location: "Westside Hub",
+    price: 800,
+    rating: 4.7,
+},
+]
