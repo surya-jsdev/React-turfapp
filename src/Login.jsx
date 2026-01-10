93b5bd9d36417c35967fe5fb96e572673b2784a9
SURYA-200346 <surya.jsdev@gmail.com>
2026-01-10 20:47:10 +0530

    login elements styled

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,28 +2,27 @@ import React from 'react'
 
 function Login() {
 
-    const style={
-        width:"350px",
-        height:"300px",
-        backgroundColor:"#0f172a"
+    const style = {
+        width: "100%",
+        height: "100vh",
+        backgroundColor: "#0f172af0",
     }
     return (
         <>
-        <div style={style}>
-            <h1>LOGIN</h1>
-
-            <form action="" className='rounded-xl'>
-                <div>
-                    <label htmlFor="email" className='block py-2 text-white'>Email:</label>
-                    <input type="text" name="email" id=""  placeholder='Enter Email...' className='border-white border-[2px] py-2 w-xs rounded-xl'/>
-                </div>
-                <div>
-                    <label htmlFor="password" className='block py-2  text-white'>Password:</label>
-                    <input type="text" name="email" id=""  placeholder='Enter Password...' className='border-white border-[2px] py-2 w-xs rounded-xl'/>
-                </div>
-                <button className='py-3 bg-[#8b5cf6] w-xs mt-5 text-white rounded-xl' >Login</button>
-            </form>
-        </div>
+            <div style={style} className='flex justify-center items-center '>
+                <form action="" className='rounded-xl w-sm bg-white h-[45vh] p-4'>
+                    <h1 className='text-center text-2xl'>LOGIN</h1>
+                    <div>
+                        <label htmlFor="email" className='block py-2 text-white'>Email:</label>
+                        <input type="text" name="email" id="" placeholder='Enter Email...' className='border-black border-[2px] py-2 w-xs rounded-xl' />
+                    </div>
+                    <div>
+                        <label htmlFor="password" className='block py-2  text-white'>Password:</label>
+                        <input type="text" name="email" id="" placeholder='Enter Password...' className='border-black border-[2px] py-2 w-xs rounded-xl' />
+                    </div>
+                    <button className='py-3 bg-[#8b5cf6] w-xs mt-5 text-white rounded-xl' >Login</button>
+                </form>
+            </div>
         </>
     )
 }
