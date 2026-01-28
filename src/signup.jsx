6cc7f4759a8b576e3493260ce06ccd33a51ac913
SURYA-200346 <surya.jsdev@gmail.com>
2026-01-28 19:43:58 +0530

    added signup state update

--- src/signup.jsx
+++ src/signup.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Signup() {
+    const navigate = useNavigate();
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
     const style = {
         width: "100%",
         height: "100vh",
@@ -16,16 +23,16 @@ function Signup() {
                     </div>
                     <div>
                         <label htmlFor="email" className='block py-4 text-[#91a0b5] self-start'>Name</label>
-                        <input type="text" name="name" id="" placeholder='name@example.com' className='border-[#303e51] border-[2px] p-3 w-xs rounded-xl bg-[#0f172a] placeholder: text-white p-2 text-sm' autoComplete='off' />
+                        <input type="text" onChange={name} name="name" id="" placeholder='name@example.com' className='border-[#303e51] border-[2px] p-3 w-xs rounded-xl bg-[#0f172a] placeholder: text-white p-2 text-sm' autoComplete='off' />
                     </div>
                     <div>
                         <label htmlFor="email" className='block py-4 text-[#91a0b5] self-start'>Email Address</label>
-                        <input type="text" name="email" id="" placeholder='name@example.com' className='border-[#303e51] border-[2px] p-3 w-xs rounded-xl bg-[#0f172a] placeholder: text-white p-2 text-sm' autoComplete='off' />
+                        <input type="text" onChange={email} name="email" id="" placeholder='name@example.com' className='border-[#303e51] border-[2px] p-3 w-xs rounded-xl bg-[#0f172a] placeholder: text-white p-2 text-sm' autoComplete='off' />
                     </div>
                     <div>
 
                         <label htmlFor="password" className='block py-4 text-[#91a0b5] self-start'>Password</label>
-                        <input type="password" name="password" id="" placeholder='Create Password' className='border-[#303e51] border-[2px] p-3 w-xs rounded-xl bg-[#0f172a] placeholder: text-white p-2 text-sm' autoComplete='off' />
+                        <input type="password" onChange={password} name="password" id="" placeholder='Create Password' className='border-[#303e51] border-[2px] p-3 w-xs rounded-xl bg-[#0f172a] placeholder: text-white p-2 text-sm' autoComplete='off' />
                     </div>
 
                     <button className='py-2 bg-[#8b5cf6] w-xs mt-8 text-white rounded-xl' >SignUp</button>
