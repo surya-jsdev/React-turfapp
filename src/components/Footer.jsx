c2d7d4c3273f00e0eb35501b33cbe7dce099372d
SURYA-200346 <surya.jsdev@gmail.com>
2026-06-12 12:59:30 +0530

    slot booking styled

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <footer className="bg-[#0f172a] text-white py-8 text-center">
             <p>&copy; 2026 360° Turf ACADEMY. All rights reserved.</p>
-            <p>Contact us at info@360degTurfacademy.com</p>
+            <p>Contact us at info@360degturfacademy.com</p>
         </footer>
     );
 }
--- src/components/SlotBooking.jsx
+++ src/components/SlotBooking.jsx
@@ -7,24 +7,24 @@ function SlotBooking() {
   const { image, name } = location.state
 
   return (
-    <>
-      <div className="p-6">
-        <h2 className="text-2xl font-bold">Slot booking</h2>
-        <p className="text-sm text-gray-400">Selected turf id: {id}</p>
+    <section className='slotbooking'>
+      <div className="p-6 w-6xl h-[650px] bg-[white] rounded-lg">
+        <h2 className="text-2xl font-bold ">Book Your Turf Slot</h2>
+        <p className="text-sm text-gray-400">Select date and time slot to book your turf</p>
         <img src={image} alt={name} className='w-xs h-40 cover rounded-md' />
 
-        <div className="grid grid-cols-3 md:grid-cols-4 gap-4 mt-10 ">
+        <div className="grid grid-cols-2 md:grid-cols-2 gap-4 mt-10 ">
           {timeslot.map((slot, index) => (
             <div
               key={index}
-              className="p-4 border rounded-lg text-center text-white cursor-pointer w-xs hover:bg-blue-500 hover:text-white transition"
+              className="w-[100px] p-4 border rounded-lg text-center text-black cursor-pointer w-xs hover:bg-blue-500 hover:text-white transition"
             >
               {slot.timeslot}
             </div>
           ))}
         </div>
       </div>
-    </>
+    </section>
   )
 }
 
