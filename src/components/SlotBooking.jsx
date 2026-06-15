843d4316e2eb4d5f20acf746b39420eb4883c7c9
SURYA-200346 <surya.jsdev@gmail.com>
2026-06-15 21:03:12 +0530

    add date choose in slotbooking

--- src/components/SlotBooking.jsx
+++ src/components/SlotBooking.jsx
@@ -10,18 +10,22 @@ function SlotBooking() {
     <section className='slotbooking'>
       <div className="p-6 w-6xl h-[650px] bg-[white] rounded-lg">
         <h2 className="text-2xl font-bold ">Book Your Turf Slot</h2>
-        <p className="text-sm text-gray-400">Select date and time slot to book your turf</p>
+        <p className="text-sm text-gray-400 pb-4">Select date and time slot to book your turf</p>
         <img src={image} alt={name} className='w-xs h-40 cover rounded-md' />
 
-        <div className="grid grid-cols-2 md:grid-cols-2 gap-4 mt-10 ">
+        <div className="w-lg grid grid-cols-2 md:grid-cols-2 gap-4 mt-10">
           {timeslot.map((slot, index) => (
             <div
               key={index}
-              className="w-[100px] p-4 border rounded-lg text-center text-black cursor-pointer w-xs hover:bg-blue-500 hover:text-white transition"
+              className="w-[200px] p-4 border rounded-lg text-center text-black cursor-pointer  hover:bg-blue-500 hover:text-white transition"
             >
               {slot.timeslot}
             </div>
+
           ))}
+        </div>
+        <div>
+          Date : <input type="date" name="" id="" />
         </div>
       </div>
     </section>
